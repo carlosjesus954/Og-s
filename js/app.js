2a9208df782289587d9334ac6dbbc179e30b9e52
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-12-26 18:43:29 +0100

    Funcionalidad al carrusel terminada

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ const opacityDiv = document.querySelector(".opacity");
 //hover para los elementos de la tienda
 const producto = document.querySelectorAll(".clothes__cards");
 
+// Funcion arrows carrusel
+const carruselContainer = document.querySelector(".section__assets")
+const arrowRight = document.querySelector(".section__right")
+const arrowLeft = document.querySelector(".section__left")
 // const imgProducto = document.query
 //  Funciones
 function mostrarMenu() {
@@ -44,6 +48,20 @@ producto.forEach((item) => {
     })
 });
 
+function moverDerecha (){
+  carruselContainer.classList.add("carrusel__deslizar--derecha")
+  carruselContainer.classList.remove("carrusel__deslizar--izquierda")
+  arrowRight.classList.add("opacity--0")
+  arrowLeft.classList.remove("opacity--0")
+}
+function moverIzquierda (){
+  carruselContainer.classList.add("carrusel__deslizar--izquierda")
+  carruselContainer.classList.remove("carrusel__deslizar--derecha")
+  arrowRight.classList.remove("opacity--0")
+  arrowLeft.classList.add("opacity--0")
+}
 // Eventos
 burguerBtn.addEventListener("click", mostrarMenu);
 closeBtn.addEventListener("click", cerrarMenu);
+arrowRight.addEventListener("click", moverDerecha);
+arrowLeft.addEventListener("click", moverIzquierda);
